27d2612f8a2acf4a5232c18773127895bd8a0ddd
dabrown <dabrown@dougUbuntu>
2017-09-12 18:45:40 -0600

    started HW 3

---
+++ HW3.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 12, 2017
+
+@author: dabrown
+'''
+
+# this is for hw3
+# this is for M4.2!! not 4.2.. MMMMM4.2
+
+# use EQ (4.109) in prob 4.11 and show y[n] converges to sqrt(alpha) as n goes infinity
+# for input x[n] = alpha * step[n]
+
+
+import numpy as np
+if __name__ == '__main__':
+    pass
+
+    
+    
